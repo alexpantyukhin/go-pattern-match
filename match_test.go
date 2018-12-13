acc89b94ba601e5b537a52720bdb6438d2c91790
apantykhin <apantykhin@gmail.com>
2018-12-14 00:21:03 +0400

    add dict

--- match_test.go
+++ match_test.go
@@ -30,6 +30,14 @@ func TestMatch_SliceWithHead(t *testing.T) {
 	assert.Equal(t, true, mr)
 }
 
+func TestMatch_SliceWithAny(t *testing.T) {
+	mr := Match([]interface{}{1, 2, 3}).
+		When([]interface{}{1, ANY, 3}, func() interface{} { return true }).
+		Result()
+
+	assert.Equal(t, true, mr)
+}
+
 func TestMatch_SliceWithValueIntType(t *testing.T) {
 	mr := Match([]int{1, 2, 3}).
 		When([]interface{}{HEAD, 2, 3}, func() interface{} { return true }).
@@ -63,6 +71,60 @@ func TestMatch_SliceHeadNotMatched(t *testing.T) {
 func TestMatch_SliceWithHeadNotMuch(t *testing.T) {
 	mr := Match([]interface{}{1, 2, 3}).
 		When([]interface{}{HEAD, 3}, func() interface{} { return true }).
+		Result()
+
+	assert.Equal(t, nil, mr)
+}
+
+func TestMatch_Map(t *testing.T) {
+	mr := Match(map[string]int{
+		"rsc": 3711,
+		"r":   2138,
+		"gri": 1908,
+		"adg": 912,
+	}).
+		When(map[string]int{
+			"rsc": 3711,
+			"r":   2138,
+			"gri": 1908,
+			"adg": 912,
+		}, func() interface{} { return true }).
+		Result()
+
+	assert.Equal(t, true, mr)
+}
+
+func TestMatch_MapPatternWithAny(t *testing.T) {
+	mr := Match(map[string]int{
+		"rsc": 3711,
+		"r":   2138,
+		"gri": 1908,
+		"adg": 912,
+	}).
+		When(map[string]interface{}{
+			"rsc": 3711,
+			"r":   2138,
+			"gri": 1908,
+			"adg": ANY,
+		}, func() interface{} { return true }).
+		Result()
+
+	assert.Equal(t, true, mr)
+}
+
+func TestMatch_MapPatternDifferentValue(t *testing.T) {
+	mr := Match(map[string]int{
+		"rsc": 3711,
+		"r":   2138,
+		"gri": 1908,
+		"adg": 912,
+	}).
+		When(map[string]interface{}{
+			"rsc": 3711,
+			"r":   2138,
+			"gri": 1908,
+			"adg": 1,
+		}, func() interface{} { return true }).
 		Result()
 
 	assert.Equal(t, nil, mr)
